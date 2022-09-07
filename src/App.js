42ae44bc900220653f6fd25f9b70f706be9cee70
omaran bazna <omran4d@gmail.com>
2022-09-06 23:23:57 -0400

    add code editor

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import logo from "./logo.svg";
 import "./App.css";
+import CodeEditor from "@uiw/react-textarea-code-editor";
 
 function App() {
-  return <div className="App"></div>;
+  const [code, setCode] = React.useState(
+    `function add(a, b) {\n  return a + b;\n}`
+  );
+  return (
+    <div className="App">
+      <CodeEditor
+        value={code}
+        language="js"
+        placeholder="Please enter JS code."
+        onChange={(evn) => setCode(evn.target.value)}
+        padding={15}
+        style={{
+          fontSize: 12,
+          backgroundColor: "#f5f5f5",
+          fontFamily:
+            "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
