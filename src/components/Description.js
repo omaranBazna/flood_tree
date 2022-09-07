1a75fb39a96416fea9c07b82e92ab58e5f196258
omaran bazna <omran4d@gmail.com>
2022-09-06 23:28:37 -0400

    add some description

---
+++ src/components/Description.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Description = () => {
+  return (
+    <div>
+      <h1>Flooding A Binary Tree </h1>
+      <div>
+        given a binary tree return a tree after flooding it with a <u>color</u>{" "}
+        starting form <u>start_node</u>
+      </div>
+
+      <h2>Example1:</h2>
+
+      <h2>Example2:</h2>
+    </div>
+  );
+};
+
+export default Description;
