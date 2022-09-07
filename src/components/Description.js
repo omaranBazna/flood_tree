26fe97f0078b8fa22d4e4f76017a7b073972eb58
omaran bazna <omran4d@gmail.com>
2022-09-06 23:33:45 -0400

    add example 1

--- src/components/Description.js
+++ src/components/Description.js
@@ -7,10 +7,19 @@ const Description = () => {
       <div>
         given a binary tree return a tree after flooding it with a <u>color</u>{" "}
         starting form <u>start_node</u>
+        The start node will be given as an array of -1's an 1's start form the
+        root [0] where 1 mean go right ,-1 mean go left it is guaranteed the
+        Tree Is Balanced if the start
       </div>
 
       <h2>Example1:</h2>
-
+      <h4>Input:</h4>
+      <p>[0,0,0] , [0] , 1</p>
+      <h4>Output:</h4>
+      <p>[1,1,1] </p>
+      <p>
+        the start node is the root ,flooding color 1 will result with this tree
+      </p>
       <h2>Example2:</h2>
     </div>
   );
